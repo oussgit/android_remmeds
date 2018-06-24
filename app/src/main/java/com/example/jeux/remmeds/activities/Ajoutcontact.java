c672d9db519555537fbfd72f8d29394740d62508
Oussama Callas <oussama.callas@edu.itescia.fr>
2018-06-24 18:56:50 +0200

    feature Ajout contact

--- app/src/main/java/com/example/jeux/remmeds/activities/Ajoutcontact.java
+++ app/src/main/java/com/example/jeux/remmeds/activities/Ajoutcontact.java
@@ -1,9 +1,14 @@
 package com.example.jeux.remmeds.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.jeux.remmeds.R;
+import com.example.jeux.remmeds.fragments.FragmentRepertoire;
 
 public class Ajoutcontact extends AppCompatActivity {
 
@@ -11,5 +16,27 @@ public class Ajoutcontact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajoutcontact);
+
+        Button ajout = findViewById(R.id.ajouter_button_layout_ajoutcontact);
+        ajout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nom = findViewById(R.id.nom_edittext_layout_ajoutcontact);
+                EditText prenom = findViewById(R.id.prenom_edittext_layout_ajoutcontact);
+                EditText num = findViewById(R.id.numero_edittext_layout_ajoutcontact);
+                EditText mail = findViewById(R.id.adresse_edittext_layout_ajoutcontact);
+
+                String contact_nom = nom.getText().toString();
+                String contact_prenom = prenom.getText().toString();
+                String contact_num = num.getText().toString();
+                String contact_mail = mail.getText().toString();
+                FragmentRepertoire.addItem(contact_nom, contact_prenom, contact_num, contact_mail);
+            }
+        });
+
     }
+
+
+
+
 }
--- app/src/main/java/com/example/jeux/remmeds/fragments/FragmentRepertoire.java
+++ app/src/main/java/com/example/jeux/remmeds/fragments/FragmentRepertoire.java
@@ -1,5 +1,6 @@
 package com.example.jeux.remmeds.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,46 +11,45 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.jeux.remmeds.activities.Ajoutcontact;
 import com.example.jeux.remmeds.classes.Contact;
 import com.example.jeux.remmeds.classes.ContactAdapter;
 import com.example.jeux.remmeds.R;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class FragmentRepertoire extends Fragment {
-    private List<Contact> contactList = new ArrayList<>();
+    private static List<Contact> contactList = new ArrayList<>();
+    private static ContactAdapter mAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         RecyclerView mRecyclerView;
-        ContactAdapter mAdapter;
-        View rep = inflater.inflate(R.layout.fragment_repertoire, container, false);
+        Button addButton;
+        final View rep = inflater.inflate(R.layout.fragment_repertoire, container, false);
+
         mRecyclerView = rep.findViewById(R.id.rep_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
         mAdapter = new ContactAdapter(contactList);
 
-        Contact contact1 = new Contact("Callas", "Oussama", "4 allée des Merisiers", "0646744246");
-        Contact contact2 = new Contact("Foucher", "Mathieu", "4 allée du Swag", "064457869");
-        Contact contact3 = new Contact("Haddouche", "Koceila", "63 rue des la marche", "0644796246");
-        Contact contact4 = new Contact("Dumont", "Adrien", "69 rue du Buffet Gargantuesque", "0142744246");
-        Contact contact6 = new Contact("Lelouche", "Giles", "159 rue Olive", "0646744246");
-        Contact contact5 = new Contact("LeCoto", "Anthony", "53 boulevard Marceau", "0425869574");
-        Contact contact7 = new Contact("Pierre", "Jean", "4 allée des Cerisiers", "0646544246");
-        contactList.add(contact1);
-        contactList.add(contact2);
-        contactList.add(contact3);
-        contactList.add(contact4);
-        contactList.add(contact5);
-        contactList.add(contact6);
-        contactList.add(contact7);
+        addButton = rep.findViewById(R.id.ajouter_button_layout_repertoire);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Ajoutcontact.class);
+                startActivity(i);
+            }
+        });
 
-        mAdapter.notifyDataSetChanged();
+
         try {
             mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
         } catch (Exception e) {
@@ -58,6 +58,13 @@ public class FragmentRepertoire extends Fragment {
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         return rep;
+
+    }
+
+    public static void addItem(String nom, String prenom, String adresse, String numero){
+        Contact contact = new Contact(nom,prenom,adresse,numero);
+        contactList.add(contact);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
