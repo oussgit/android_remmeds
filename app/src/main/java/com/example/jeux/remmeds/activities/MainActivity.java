059cb7c62c9107a59472dea8800c81a0d7c805d7
Mathieu Foucher <grattemoi@gmail.com>
2018-07-10 11:45:58 +0200

    Suppression page Debug
    - Pour le moment, le menu est encore présent

--- app/src/main/java/com/example/jeux/remmeds/activities/MainActivity.java
+++ app/src/main/java/com/example/jeux/remmeds/activities/MainActivity.java
@@ -155,7 +155,7 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
 
-            Intent intent = new Intent(this, Debug.class);
+            Intent intent = new Intent(this, Authentification.class);
 
             startActivity(intent);
         }
